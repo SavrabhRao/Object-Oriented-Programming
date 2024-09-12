b60ff3e953c2ebccb66aeddbee788489ef31f50c
SavrabhRao <121105395+SavrabhRao@users.noreply.github.com>
2024-09-12 23:40:44 +0530

    Update test33.py

--- test33.py
+++ test33.py
@@ -7,14 +7,14 @@ class neha:
         print("hi this is neha")
 
 class nehasson(neha):
-    def __init__(self,vagheshentry):
+    def __init__(self,marriage):
         print("hello this is neha")
-        if vagheshentry.lower() == "yes":
+        if marriage.lower() == "yes":
             super().__init__()
         else:
             print("neha have no kids")
     def intro(self):
-        print("hi i am neha and vag**sh's kid")
+        print("hi i am neha's kid")
 
 saurabh = nehasson("yes")
 saurabh.intro()
