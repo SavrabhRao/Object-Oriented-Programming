4ac7fd7bf7a104256a331847f355cef777978c08
SavrabhRao <121105395+SavrabhRao@users.noreply.github.com>
2024-09-12 23:35:57 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+# class Planning:
+#     def movie(self,moviename):
+#         print("i will watch: ",moviename," tonight")
+#     def parents(self):
+#         print("parents forget to keep keys: ")
+
+# tue = Planning()
+# tue.movie("djtillu2")
+# tue.parents()
+
+# l = [1,3,4,2,6,8,7]
+# l.sort()
+# print(l)
+
+class test:
+    def __init__(self):
+        print("object is created")
+        self.a = 10
+
+    def display(self):
+        print(self.a)
+
+t = test()
+print(t)
+
